e865c07081211cc961dfa84c031813227f9551ab
heysaladin <didien.geonk@gmail.com>
2022-01-02 12:57:27 +0700

    init hf

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -12,6 +12,14 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
@@ -40,4 +48,36 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // user has many posts
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Posts', 'author_id');
+    }
+
+    // user has many comments
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comments', 'from_user');
+    }
+
+    public function can_post()
+    {
+        $role = $this->role;
+        if ($role == 'author' || $role == 'admin') {
+        return true;
+        }
+        return true;
+    }
+
+    public function is_admin()
+    {
+        $role = $this->role;
+        if ($role == 'admin') {
+        return true;
+        }
+        return false;
+    }
+
 }
