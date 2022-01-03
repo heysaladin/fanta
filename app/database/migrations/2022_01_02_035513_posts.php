1e68c6133ffd118024f5374f34789f8bbdbe537b
heysaladin <didien.geonk@gmail.com>
2022-01-03 11:01:45 +0700

    support author

--- app/database/migrations/2022_01_02_035513_posts.php
+++ app/database/migrations/2022_01_02_035513_posts.php
@@ -41,6 +41,9 @@ class Posts extends Migration
             // $table->unsignedBigInteger('category_id');
             // $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
 
+            $table->unsignedBigInteger('support_author_id')->nullable();
+            $table->foreign('support_author_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->softDeletes();
         });
     }
